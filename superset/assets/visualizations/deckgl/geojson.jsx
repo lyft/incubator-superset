f9881dc5248285b2d77547b2a7b6cd2c64a091fa
Hugh Miles <hmiles@lyft.com>
2017-12-15 08:41:16 -0800

    refactor on geojson.jsx

--- superset/assets/visualizations/deckgl/geojson.jsx
+++ superset/assets/visualizations/deckgl/geojson.jsx
@@ -5,68 +5,21 @@ import { GeoJsonLayer } from 'deck.gl';
 import DeckGLContainer from './DeckGLContainer';
 
 function DeckGeoJsonLayer(slice, payload, setControlValue) {
-  console.log(payload)
   const fd = slice.formData;
   const c = fd.color_picker;
-  // const data = payload.data.features.map(d => ({
-  //   ...d,
-  //   color: [c.r, c.g, c.b, 255 * c.a],
-  // }));
-
-  const sample_geojson_data = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -122.43335723876952,
-          37.784282779035216
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -122.4213409423828,
-              37.76895071032235
-            ],
-            [
-              -122.39713668823241,
-              37.76895071032235
-            ],
-            [
-              -122.39713668823241,
-              37.791337175930686
-            ],
-            [
-              -122.4213409423828,
-              37.791337175930686
-            ],
-            [
-              -122.4213409423828,
-              37.76895071032235
-            ]
-          ]
-        ]
-      }
-    }
-  ]
-}
+  const data = payload.data.geojson.features.map(d => {
+    d.properties.fillColor =  [c.r, c.g, c.b, 255 * c.a]
+    d.properties.elevation = 3000
+    return d
+  });
 
   const layer = new GeoJsonLayer({
     id: `geojson-layer`,
-    data: sample_geojson_data,
+    data,
     filled: true,
     stroked: false,
     extruded: true,
+    pointRadiusScale: 100,
   });
 
   const viewport = {
