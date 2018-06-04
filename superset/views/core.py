b955cf4254a4befb9d1142ade7586ddd6fb54264
Mahendra <mahendra@affirm.com>
2018-06-03 18:06:34 -0700

    [scheduled reports] Add support for scheduled reports
    * Scheduled email reports for slice and dashboard visualization
      (attachment or inline)
    * Scheduled email reports for slice data (CSV attachment on inline table)
    * Each schedule has a list of recipients (all of them can receive a single mail,
      or separate mails)
    * All outgoing mails can have a mandatory bcc - for audit purposes.
    * Each dashboard/slice can have multiple schedules.
    
    In addition, this PR also makes a few minor improvements to the celery
    infrastructure.
    * Create a common celery app
    * Added more celery annotations for the tasks
    * Introduced celery beat
    * Update docs about concurrency / pools

--- superset/views/core.py
+++ superset/views/core.py
@@ -1638,7 +1638,6 @@ class Superset(BaseSupersetView):
     @staticmethod
     def _set_dash_metadata(dashboard, data):
         positions = data['positions']
-
         # find slices in the position data
         slice_ids = []
         slice_id_to_name = {}
