b3f5c82d18226ce1fe40d14819ed47dec98935f7
Beto Dealmeida <roberto@dealmeida.net>
2018-06-25 16:18:58 -0700

    DB model and migration

---
+++ superset/migrations/versions/a190c39de85b_add_implicit_tags.py
@@ -0,0 +1,205 @@
+"""Add implicit tags
+
+Revision ID: a190c39de85b
+Revises: bddc498dd179
+Create Date: 2018-06-20 16:07:49.124887
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'a190c39de85b'
+down_revision = 'bddc498dd179'
+
+from alembic import op
+from flask_appbuilder.models.mixins import AuditMixin
+import sqlalchemy as sa
+from sqlalchemy import Column, Enum, Integer, ForeignKey, String, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from superset import db
+from superset.models.tags import ObjectTypes, TagTypes
+
+
+Base = declarative_base()
+
+
+class Tag(Base):
+    """A tag attached to an object (query, chart or dashboard)."""
+    __tablename__ = 'tag'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), unique=True)
+    type = Column(Enum(TagTypes))
+
+
+class TaggedObject(Base):
+    __tablename__ = 'tagged_object'
+
+    id = Column(Integer, primary_key=True)
+    tag_id = Column(Integer, ForeignKey('tag.id'))
+    object_id = Column(Integer)
+    object_type = Column(Enum(ObjectTypes))
+
+
+class User(Base):
+    """Declarative class to do query in upgrade"""
+    __tablename__ = 'ab_user'
+    id = Column(Integer, primary_key=True)
+
+
+slice_user = Table(
+    'slice_user',
+    Base.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('ab_user.id')),
+    Column('slice_id', Integer, ForeignKey('slices.id'))
+)
+
+
+dashboard_user = Table(
+    'dashboard_user',
+    Base.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('ab_user.id')),
+    Column('dashboard_id', Integer, ForeignKey('dashboards.id'))
+)
+
+
+class Slice(Base, AuditMixin):
+    """Declarative class to do query in upgrade"""
+    __tablename__ = 'slices'
+
+    id = Column(Integer, primary_key=True)
+    owners = relationship("User", secondary=slice_user)
+
+
+class Dashboard(Base, AuditMixin):
+    """Declarative class to do query in upgrade"""
+    __tablename__ = 'dashboards'
+    id = Column(Integer, primary_key=True)
+    owners = relationship("User", secondary=dashboard_user)
+
+
+class Query(Base):
+    __tablename__ = 'query'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('ab_user.id'))
+
+
+class Favstar(Base):
+    __tablename__ = 'favstar'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('ab_user.id'))
+    class_name = Column(String(50))
+    obj_id = Column(Integer)
+
+
+def get_tag(name, session, type):
+    try:
+        tag = session.query(Tag).filter_by(name=name, type=type).one()
+    except Exception:
+        tag = Tag(name=name, type=type)
+        session.add(tag)
+        session.commit()
+
+    return tag
+
+
+def get_object_type(class_name):
+    mapping = {
+        'slice': ObjectTypes.chart,
+        'dashboard': ObjectTypes.dashboard,
+    }
+    try:
+        return mapping[class_name.lower()]
+    except KeyError:
+        raise Exception('No mapping found for {0}'.format(class_name))
+
+
+def upgrade():
+    bind = op.get_bind()
+    session = db.Session(bind=bind)
+
+    Tag.__table__.create(bind)
+    TaggedObject.__table__.create(bind)
+
+    # add type tags (eg, `type:dashboard` for dashboards)
+    for type in ObjectTypes.__members__:
+        session.add(Tag(name='type:{0}'.format(type), type=TagTypes.type))
+
+    # add owner tags (eg, `owner:1` for things owned by the admin)
+    for chart in session.query(Slice):
+        for owner in chart.owners:
+            name = 'owner:{0}'.format(owner.id)
+            tag = get_tag(name, session, TagTypes.owner)
+            tagged_object = TaggedObject(
+                tag_id=tag.id,
+                object_id=chart.id,
+                object_type=ObjectTypes.chart,
+            )
+            session.add(tagged_object)
+
+            tag = get_tag('type:chart', session, TagTypes.type)
+            tagged_object = TaggedObject(
+                tag_id=tag.id,
+                object_id=chart.id,
+                object_type=ObjectTypes.chart,
+            )
+            session.add(tagged_object)
+
+
+    for dashboard in session.query(Dashboard):
+        for owner in dashboard.owners:
+            name = 'owner:{0}'.format(owner.id)
+            tag = get_tag(name, session, TagTypes.owner)
+            tagged_object = TaggedObject(
+                tag_id=tag.id,
+                object_id=dashboard.id,
+                object_type=ObjectTypes.dashboard,
+            )
+            session.add(tagged_object)
+
+            tag = get_tag('type:dashboard', session, TagTypes.type)
+            tagged_object = TaggedObject(
+                tag_id=tag.id,
+                object_id=dashboard.id,
+                object_type=ObjectTypes.dashboard,
+            )
+            session.add(tagged_object)
+
+    for query in session.query(Query):
+        name = 'owner:{0}'.format(query.user_id)
+        tag = get_tag(name, session, TagTypes.owner)
+        tagged_object = TaggedObject(
+            tag_id=tag.id,
+            object_id=query.id,
+            object_type=ObjectTypes.query,
+        )
+        session.add(tagged_object)
+
+        tag = get_tag('type:query', session, TagTypes.type)
+        tagged_object = TaggedObject(
+            tag_id=tag.id,
+            object_id=query.id,
+            object_type=ObjectTypes.query,
+        )
+        session.add(tagged_object)
+
+    # add favorited_by tags
+    for star in session.query(Favstar):
+        name = 'favorited_by:{0}'.format(star.user_id)
+        tag = get_tag(name, session, TagTypes.favorited_by)
+        tagged_object = TaggedObject(
+            tag_id=tag.id,
+            object_id=star.obj_id,
+            object_type=get_object_type(star.class_name),
+        )
+        session.add(tagged_object)
+
+    session.commit()
+
+
+def downgrade():
+    op.drop_table('tag')
+    op.drop_table('tagged_object')
