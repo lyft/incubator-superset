548f75b4ef7ae0da1a79994d1793bc131c3b3312
Beto Dealmeida <roberto@dealmeida.net>
2018-08-10 09:40:21 -0700

    Landing page
    (cherry picked from commit 6eafd0bda82d663504a51e51f50648c6d510b4ba)

--- superset/views/__init__.py
+++ superset/views/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+from . import lyft # noqa
 from . import base  # noqa
 from . import core  # noqa
 from . import sql_lab  # noqa
-from . import lyft # noqa
 from . import annotations # noqa
 from . import datasource # noqa
+from . import tags  # noqa
