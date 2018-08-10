e3a562176ef2883f84f1e031b7d2f897ef5d8e0e
Beto Dealmeida <roberto@dealmeida.net>
2018-08-10 09:40:21 -0700

    Landing Page (frontend only)
    (cherry picked from commit 46487c588d76b78aaa83c39aba3c876eb6c674a7)
    (cherry picked from commit 18e944a382a5cf867020b5d31e90144aa7a3c07e)
    (cherry picked from commit a8ac3fbe89b738301bd0d9b2f238de40a1826124)
    (cherry picked from commit 55b7eb6245dc7d877eaf00b7cf0d849ca5d29c2e)
    
    Rebase
    
    Fix tag endpoint
    
    (cherry picked from commit d714792cad0c71af3fabca2cc6c5b741f046a970)
    (cherry picked from commit 8181c540f76eacd87d779f850d309a3bf101424a)
    (cherry picked from commit e82c6150517cb1fb91b3ba34e2d4aa1b1f5437a7)
    (cherry picked from commit 8cd80a25d6d26a59e977f08b7430d279e78ab0f0)
    (cherry picked from commit 2ba0deeaeff8200bb90c059c9da7d4784faf0423)

--- superset/assets/src/dashboard/components/BuilderComponentPane.jsx
+++ superset/assets/src/dashboard/components/BuilderComponentPane.jsx
@@ -29,6 +29,7 @@ import NewDivider from './gridComponents/new/NewDivider';
 import NewHeader from './gridComponents/new/NewHeader';
 import NewRow from './gridComponents/new/NewRow';
 import NewTabs from './gridComponents/new/NewTabs';
+import NewTags from './gridComponents/new/NewTags';
 import NewMarkdown from './gridComponents/new/NewMarkdown';
 import SliceAdder from '../containers/SliceAdder';
 
@@ -111,6 +112,7 @@ class BuilderComponentPane extends React.PureComponent {
                         <NewHeader />
                         <NewMarkdown />
                         <NewDivider />
+                        <NewTags />
                       </div>
                       <div className="slices-layer slide-content">
                         <div
