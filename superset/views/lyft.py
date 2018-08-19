818997e8ba44736fadd577756efa2d0a8f3d1556
Beto Dealmeida <roberto@dealmeida.net>
2018-08-19 15:26:54 -0700

    Fix bug
    (cherry picked from commit 1224d8f76607b49f1c029bd207619172a244fba5)

--- superset/views/lyft.py
+++ superset/views/lyft.py
@@ -1,20 +1,35 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=broad-except,no-self-use
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from flask import Response, request, g
+import os
+import logging
+from flask import Response, request
+from flask_babel import gettext as __
 from flask_appbuilder import expose
+from flask_appbuilder.security.decorators import has_access_api
 
-from superset import app, appbuilder, security_manager
+from superset import app, appbuilder, utils
 import superset.models.core as models
 from superset.views.core import Superset
-from .base import json_error_response
 
 config = app.config
 stats_logger = config.get('STATS_LOGGER')
 log_this = models.Log.log_this
 DAR = models.DatasourceAccessRequest
+
+
+ALL_DATASOURCE_ACCESS_ERR = __(
+    'This endpoint requires the `all_datasource_access` permission')
+DATASOURCE_MISSING_ERR = __('The datasource seems to have been deleted')
+ACCESS_REQUEST_MISSING_ERR = __(
+    'The access requests seem to have been deleted')
+USER_MISSING_ERR = __('The user seems to have been deleted')
+DATASOURCE_ACCESS_ERR = __("You don't have access to this datasource")
+SECRET_KEY = os.getenv("CREDENTIALS_SUPERSET_SECRET_KEY") or None
 
 
 def json_success(json_msg, status=200):
@@ -23,26 +38,19 @@ def json_success(json_msg, status=200):
 
 class Lyft(Superset):
 
-    def authorize(self):
-        """Provides access if token, impersonates if specified"""
-        if not security_manager.has_tom_key():
-            raise Exception("Wrong key")
+    def datasource_access(self, datasource, user=None):
+        if SECRET_KEY is None:
+            logging.error('No secret loaded')
+            return False
 
-        email = request.headers.get('IMPERSONATE')
-        if email:
-            user = security_manager.find_user(email=email)
-            if not user:
-                raise Exception("Email to impersonate not found")
-            g.user = user
+        tom_request_key = request.headers.get('TOM_ACCESS_KEY')
+        return tom_request_key == SECRET_KEY
 
+    @has_access_api
     @expose('/sql_json/', methods=['POST', 'GET'])
     @log_this
     def sql_json(self):
-        try:
-            self.authorize()
-        except Exception as e:
-            return json_error_response('{}'.format(e))
-        return self.sql_json_call(request)
+        return super(Lyft, self).sql_json()
 
 
 appbuilder.add_view_no_menu(Lyft)
