32592d54971a73c3aa63831cc1386d5eb47b55b3
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-09-13 08:35:09 -0700

    cherry_merge_heads

---
+++ superset/migrations/versions/049713877a13_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 049713877a13
+Revises: ('55e910a74826', '179ef0f46016')
+Create Date: 2018-09-13 08:34:48.146332
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '049713877a13'
+down_revision = ('55e910a74826', '179ef0f46016')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
