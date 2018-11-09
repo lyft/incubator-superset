107a98de7a62d649f818180e1083b6a6e062ca20
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-11-09 08:46:06 -0800

    merging heads
    (cherry picked from commit 399bfb9c61a3639fb6fdf3c1bc7d13e07844c6de)

---
+++ superset/migrations/versions/750a37bd94c9_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 750a37bd94c9
+Revises: ('55e910a74826', '179ef0f46016')
+Create Date: 2018-11-09 08:45:30.499196
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '750a37bd94c9'
+down_revision = ('55e910a74826', '179ef0f46016')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
