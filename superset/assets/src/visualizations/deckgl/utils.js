177e37830d39d352a3f30c41f4a88462235acc08
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-11-15 01:08:46 +0000

    Fix adhoc metrics in Polygon
    (cherry picked from commit b55fbe4cefd37ee7708bd30fe78bbb7bf3bd21f1)
    (cherry picked from commit b1d213dcd7e483ab64b031224462df6440014b0e)

--- superset/assets/src/visualizations/deckgl/utils.js
+++ superset/assets/src/visualizations/deckgl/utils.js
@@ -6,17 +6,14 @@ import { hexToRGB } from '../../modules/colors';
 export function getBreakPoints({
     break_points: formDataBreakPoints,
     num_buckets: formDataNumBuckets,
-    metric,
-  }, features) {
+  }, features, accessor) {
   if (!features) {
     return [];
   }
   if (formDataBreakPoints === undefined || formDataBreakPoints.length === 0) {
     // compute evenly distributed break points based on number of buckets
-    const numBuckets = formDataNumBuckets
-      ? parseInt(formDataNumBuckets, 10)
-      : 10;
-    const [minValue, maxValue] = extent(features, d => d[metric]);
+    const numBuckets = formDataNumBuckets ? parseInt(formDataNumBuckets, 10) : 10;
+    const [minValue, maxValue] = extent(features, accessor);
     const delta = (maxValue - minValue) / numBuckets;
     const precision = delta === 0
       ? 0
@@ -32,15 +29,13 @@ export function getBreakPointColorScaler({
     break_points: formDataBreakPoints,
     num_buckets: formDataNumBuckets,
     linear_color_scheme: linearColorScheme,
-    metric,
     opacity,
-  }, features) {
+  }, features, accessor) {
   const breakPoints = formDataBreakPoints || formDataNumBuckets
     ? getBreakPoints({
       break_points: formDataBreakPoints,
       num_buckets: formDataNumBuckets,
-      metric,
-    }, features)
+    }, features, accessor)
     : null;
   const colorScheme = Array.isArray(linearColorScheme)
     ? new SequentialScheme({
@@ -69,13 +64,14 @@ export function getBreakPointColorScaler({
     maskPoint = value => value > breakPoints[n] || value < breakPoints[0];
   } else {
     // interpolate colors linearly
-    scaler = colorScheme.createLinearScale(extent(features, d => d[metric]));
+    scaler = colorScheme.createLinearScale(extent(features, accessor));
     maskPoint = () => false;
   }
 
   return (d) => {
-    const c = hexToRGB(scaler(d[metric]));
-    if (maskPoint(d[metric])) {
+    const v = accessor(d);
+    const c = hexToRGB(scaler(v));
+    if (maskPoint(v)) {
       c[3] = 0;
     } else {
       c[3] = (opacity / 100.0) * 255;
@@ -84,15 +80,15 @@ export function getBreakPointColorScaler({
   };
 }
 
-export function getBuckets(fd, features) {
-  const breakPoints = getBreakPoints(fd, features, true);
-  const colorScaler = getBreakPointColorScaler(fd, features);
+export function getBuckets(fd, features, accessor) {
+  const breakPoints = getBreakPoints(fd, features, accessor);
+  const colorScaler = getBreakPointColorScaler(fd, features, accessor);
   const buckets = {};
   breakPoints.slice(1).forEach((value, i) => {
     const range = breakPoints[i] + ' - ' + breakPoints[i + 1];
     const mid = 0.5 * (parseInt(breakPoints[i], 10) + parseInt(breakPoints[i + 1], 10));
     buckets[range] = {
-      color: colorScaler({ [fd.metric]: mid }),
+      color: colorScaler({ [fd.metric.label || fd.metric]: mid }),
       enabled: true,
     };
   });
