3b6ff8abcea5ef434dec01ab2c59f00f329317f0
Beto Dealmeida <roberto@dealmeida.net>
2019-01-17 09:16:35 -0800

    Trim query before parsing (#6703)
    * Ignore empty lines
    
    * Fix unit tests
    
    * Use stripped
    
    * Fix another unit test
    
    (cherry picked from commit 99d6dc07d6910ae5c5fe0d5f98e993bc960dff73)

--- tests/db_engine_specs_test.py
+++ tests/db_engine_specs_test.py
@@ -180,8 +180,7 @@ class DbEngineSpecsTestCase(SupersetTestCase):
             FROM
             table
             LIMIT 99990""",
-            """
-            SELECT
+            """SELECT
                 'LIMIT 777' AS a
                 , b
             FROM
@@ -198,13 +197,12 @@ class DbEngineSpecsTestCase(SupersetTestCase):
                 FROM
                 table
                 LIMIT         99990            ;""",
-            """
-                SELECT
+            """SELECT
                     'LIMIT 777' AS a
                     , b
                 FROM
                 table
-                LIMIT         1000            ;""",
+                LIMIT         1000""",
         )
 
     def test_get_datatype(self):
@@ -222,8 +220,7 @@ class DbEngineSpecsTestCase(SupersetTestCase):
                 FROM
                 table
                 LIMIT 99990, 999999""",
-            """
-                SELECT
+            """SELECT
                     'LIMIT 777' AS a
                     , b
                 FROM
@@ -241,8 +238,7 @@ class DbEngineSpecsTestCase(SupersetTestCase):
                 table
                 LIMIT 99990
                 OFFSET 999999""",
-            """
-                SELECT
+            """SELECT
                     'LIMIT 777' AS a
                     , b
                 FROM
